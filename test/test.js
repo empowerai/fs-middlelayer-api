8bf57b5e5d425531153f76ec0d2ae7ca03f80d92
Brian Funk <bfunk@nciinc.com>
2017-02-15 21:36:22 -0500

    initial api unit tests - json, html, 404, headers

---
+++ test/test.js
@@ -0,0 +1,71 @@
+/*
+
+  ___ ___       ___               _ _       _   ___ ___ 
+ | __/ __|  ___| _ \___ _ _ _ __ (_) |_    /_\ | _ \_ _|
+ | _|\__ \ / -_)  _/ -_) '_| '  \| |  _|  / _ \|  _/| | 
+ |_| |___/ \___|_| \___|_| |_|_|_|_|\__| /_/ \_\_| |___|
+
+*/
+
+//*******************************************************************
+
+'use strict';
+
+//*******************************************************************
+
+var request = require('supertest');
+var server = require('../index.js');
+
+var chai = require('chai');
+var expect = chai.expect;
+var assert = chai.assert;
+var should = chai.should;
+
+//*******************************************************************
+
+describe('FS ePermit API', function() {
+    
+    it('should return html format if web page', function(done) {
+
+        request(server)
+            .get('/')
+            .expect('Content-Type', /html/)
+            .expect(200, done);
+    });
+    
+    it('should return json format if api', function(done) {
+
+        request(server)
+            .get('/api')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+    
+    it('should return a 404 error if invalid', function(done) {
+        request(server)
+            .get('/asdfsad')
+            .expect(404, done);
+    });
+    
+    it('should not have x-powered-by header', function(done) {
+        request(server)
+            .get('/api')
+            .expect(function(res) {
+                expect(res.headers).to.not.have.key('x-powered-by');
+            })
+            .expect(200, done);
+    });
+    
+    it('should have cors support', function(done) {
+        request(server)
+            .get('/api')
+            .expect(function(res) {
+                expect(res.headers['access-control-allow-origin']).to.equal('*');
+            })
+            .expect(200, done);
+    });
+     
+});
+
+//*******************************************************************
+
