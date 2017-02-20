1813b1b5f4b1b6ef425b3fa13a9bb487b77a57f5
Brian Funk <bfunk@nciinc.com>
2017-02-20 17:17:58 -0500

    add initial controller files

---
+++ controllers/permits/special-uses/non-commercial/index.js
@@ -0,0 +1,69 @@
+/*
+
+  ___ ___       ___               _ _       _   ___ ___ 
+ | __/ __|  ___| _ \___ _ _ _ __ (_) |_    /_\ | _ \_ _|
+ | _|\__ \ / -_)  _/ -_) '_| '  \| |  _|  / _ \|  _/| | 
+ |_| |___/ \___|_| \___|_| |_|_|_|_|\__| /_/ \_\_| |___|
+
+*/
+
+//*******************************************************************
+
+'use strict';
+
+//*******************************************************************
+// required modules
+
+var include = require('include')(__dirname);
+
+//*******************************************************************
+// mock data
+var mock = {};
+
+mock.non_commercial_get_id = include('test/data/non-commercial.get.id.json');
+
+//*******************************************************************
+// controller
+
+var get = {};
+var put = {};
+var post;
+
+// get all
+
+get.all = function(req,res){
+
+    res.json(include('test/data/non-commercial.get.all.json'));
+
+};
+
+// get id
+
+get.id = function(req,res){
+    
+    res.json(include('test/data/non-commercial.get.id.json'));
+
+};
+
+// put id
+
+put.id = function(req,res){
+    
+    res.json(include('test/data/non-commercial.put.id.json'));
+
+};
+
+// post
+
+post = function(req,res){
+    
+    res.json(include('test/data/non-commercial.post.json'));
+
+};
+
+//*******************************************************************
+// exports
+
+module.exports.get = get;
+module.exports.put = put;
+module.exports.post = post;
