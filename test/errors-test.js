cadc0a88b925b37ddb5c9e9931d0dba6abd66f7d
cwolf1038 <cwolf123456@yahoo.com>
2017-02-22 19:13:20 -0500

    adding test for error outputting function.

---
+++ test/errors-test.js
@@ -0,0 +1,43 @@
+/*
+
+  ___ ___       ___               _ _       _   ___ ___ 
+ | __/ __|  ___| _ \___ _ _ _ __ (_) |_    /_\ | _ \_ _|
+ | _|\__ \ / -_)  _/ -_) '_| '  \| |  _|  / _ \|  _/| | 
+ |_| |___/ \___|_| \___|_| |_|_|_|_|\__| /_/ \_\_| |___|
+
+*/
+
+//*******************************************************************
+
+'use strict';
+
+//*******************************************************************
+
+var request = require('supertest');
+var server = require('../index.js');
+
+var chai = require('chai');
+var expect = chai.expect;
+var assert = chai.assert;
+var should = chai.should;
+
+var non_commercial = {};
+
+non_commercial.validate = require('../error.js');
+
+//*******************************************************************
+
+describe('Error function: sendError', function() {
+    
+    it('should return valid json and a status code of 400', function(done) {
+        request(server)
+            .get('/permits/special-uses/noncommercial/errorTest')
+            .expect('Content-Type', /json/)
+            .expect(400, done);
+    });
+
+});
+
+
+
+//*******************************************************************
