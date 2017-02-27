a3750cdb3a10c7eb3f33cf056440e38a053b0099
cwolf1038 <cwolf123456@yahoo.com>
2017-02-27 12:23:07 -0500

    Added tests for error message building function

--- test/controllers-test.js
+++ test/controllers-test.js
@@ -21,6 +21,7 @@ var should = chai.should;
 var special_uses = {};
 
 special_uses.validate = require('../controllers/permits/special-uses/validate.js');
+special_uses.build_error_message = require('../controllers/permits/special-uses/utility.js');
 
 //*******************************************************************
 
@@ -38,6 +39,20 @@ describe('API Controllers: validate GET permitId', function() {
         expect( special_uses.validate.permit_id(1234567890) ).to.be.equal(true);
     });
 
+});
+
+describe('API Controllers: build error message',function(){
+
+    it('should return "firstName is a required field!"',function(){
+        var errors = ['firstName'];
+        expect( special_uses.build_error_message.build_error_message(errors) ).to.be.equal('firstName is a required field!');
+    });
+
+    it('should return "firstName and lastName are required fields!"',function(){
+        var errors = ['firstName', 'lastName'];
+        expect( special_uses.build_error_message.build_error_message(errors) ).to.be.equal('firstName and lastName are required fields!');
+    });
+    
 });
 
 //*******************************************************************
