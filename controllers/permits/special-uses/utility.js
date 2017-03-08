cee46a86eebf168b19497da3ac6bb42216a2eb9d
cameronwolf <cameronwolf@github.com>
2017-03-08 12:44:37 -0500

    Updated error building functionality.

--- controllers/permits/special-uses/utility.js
+++ controllers/permits/special-uses/utility.js
@@ -18,7 +18,7 @@ var _ = require("lodash");
 
 //*******************************************************************
 
-function build_error_message(error_array){
+function build_missing_error_message(error_array){
 
     var error_message = _.join(error_array, " and ");
 
@@ -37,17 +37,65 @@ function build_error_message(error_array){
 
 }
 
-var invalid_field = function invalid_field(output, field){
+function build_type_error_message(type_obj){
+
+    var error_message = type_obj.field + " is expected to be type '" + type_obj.expected_type + "'.";
+    return error_message;
+
+}
+
+function build_error_message(output){
+
+    var missing_message = "", type_message = "", error_message = "";
+
+    if (!_.isEmpty(output.missing_array)){
+
+        missing_message = build_missing_error_message(output.missing_array);
+        error_message = error_message + missing_message;
+
+    }
+    if (!_.isEmpty(output.type_array)){
+
+        output.type_array.forEach((element)=>{
+
+            type_message = type_message + build_type_error_message(element) + " ";
+
+        });
+        error_message = error_message + type_message;
+
+    }
+    
+    return error_message;
+
+}
+
+var invalid_field = function (output, field){
     
     output.fields_valid = false;
-    output.error_array.push(field);
+    output.missing_array.push(field);
 
     return output;
 
 };
+
+var field_type = function (output, field, expected_type){
+    
+    output.fields_valid = false;
+    output.type_array.push({
+
+        "field":field,
+        "expected_type":expected_type
+
+    });
+
+    return output;
+
+};
+
 
 //*******************************************************************
 // exports
 
 module.exports.build_error_message = build_error_message;
 module.exports.invalid_field = invalid_field;
+module.exports.field_type = field_type;
