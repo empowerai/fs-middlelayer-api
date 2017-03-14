4f3673f74512c60ca82e20c062e4d83590d177ad
cwolf1038 <cwolf123456@yahoo.com>
2017-03-14 12:00:32 -0400

    Update tests.

--- test/permits/special-uses/commercial/outfitters/outfitters.js
+++ test/permits/special-uses/commercial/outfitters/outfitters.js
@@ -212,7 +212,7 @@ describe('outfitters POST: validate required fields present', function(){
 				.expect('Content-Type', /json/)
 				.expect(function(res){
 
-					expect(res.body.response.message).to.equal('Applicant Info/Day Phone is a required field.');
+					expect(res.body.response.message).to.equal('Applicant Info/Day Phone/Area Code is a required field. Applicant Info/Day Phone/Number is a required field. Applicant Infoday Phone/Type is a required field.');
 
 				})
 				.expect(400, done);
