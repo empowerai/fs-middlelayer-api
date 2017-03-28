6e62ec82732c495e9242cbfec0067df7d0608d98
cwolf1038 <cwolf123456@yahoo.com>
2017-03-27 20:13:08 -0400

    Added tests to ensure invalid PUT requests are handled correctly.

--- test/permits/applications/special-uses/commercial/outfitters/outfitters.js
+++ test/permits/applications/special-uses/commercial/outfitters/outfitters.js
@@ -496,6 +496,40 @@ describe('API Routes: permits/special-uses/commercial/outfitters', function() {
 
 });
 
+describe('outfitters PUT: field type validated', function(){
+
+	let token;
+
+	before(function(done) {
+
+		util.getToken(function(t){
+
+			token = t;
+			return done();
+
+		});
+	
+	});
+
+	describe('outfitters PUT: required fields are type validated', function(){
+
+		it('should return valid json with error if no updated provided', function(done) {
+
+			request(server)
+				.put('/permits/applications/special-uses/commercial/outfitters/123')
+				.set('x-access-token', token)
+				.expect('Content-Type', /json/)
+				.expect(function(res){
+
+					expect(res.body.response.message).to.equal('Region is a required field. Forest is a required field. District is a required field. Applicant Info/First Name is a required field. Applicant Info/Last Name is a required field. Applicant Info/Day Phone/Area Code is a required field. Applicant Info/Day Phone/Number is a required field. Applicant Info/Day Phone/Type is a required field. Applicant Info/Email Address is a required field. Applicant Info/Mailing Address is a required field. Applicant Info/Mailing City is a required field. Applicant Info/Mailing Zip is a required field. Applicant Info/Mailing State is a required field. Applicant Info/Org Type is a required field. Type is a required field. Temp Outfitter Fields/Activity Description is a required field. Temp Outfitter Fields/Location Description is a required field. Temp Outfitter Fields/Start Date Time is a required field. Temp Outfitter Fields/End Date Time is a required field. Temp Outfitter Fields/Insurance Certificate is a required field. Temp Outfitter Fields/Good Standing Evidence is a required field. Temp Outfitter Fields/Operating Plan is a required field.');
+
+				})
+				.expect(400, done);
+
+		});
+	});
+});
+
 describe('outfitters POST: field type validated', function(){
 
 	let token;
@@ -963,7 +997,7 @@ describe('outfitters POST: enum validated', function(){
 				.expect('Content-Type', /json/)
 				.expect(function(res){
 
-					expect(res.body.response.message).to.equal('Type is not one of enum values: noncommercial,tempOutfitterGuide.');
+					expect(res.body.response.message).to.equal('Type is not one of enum values: noncommercial,tempOutfitters.');
 
 				})
 				.expect(400, done);
