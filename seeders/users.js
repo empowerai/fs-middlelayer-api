159ab61dec112032ff68ddb7d15f6aa41e5ae1fb
Shekar <spendem@computechinc.com>
2017-04-03 17:02:04 -0400

    users moved to the env

--- seeders/users.js
+++ seeders/users.js
@@ -1,9 +1,12 @@
 'use strict';
+
+require('dotenv').config();
+
 module.exports = {
 	up: function (queryInterface, Sequelize) { 
 		return queryInterface.bulkInsert('users', [
-		        {user_name: "admin@fs.fed.us", pass_hash: "$2a$10$6VvFcDU3QnOriEOD1CgzsuED7HntnP484S89tSeZwYZWJs90biyUW", user_role: "admin", created: "now()", updated: "now()" }, 
-		        {user_name: "user@fs.fed.us", pass_hash: "$2a$10$Mkhnuk8VFzwSyJ0zgSoeMuZS27K75Dzeaz6gVyjFh9GMW6dXxqBnq", user_role: "user", created: "now()", updated: "now()" } 
+		        {user_name: "admin@fs.fed.us", pass_hash: process.env.ADMINROLE_HASH, user_role: "admin", created: "now()", updated: "now()" }, 
+		        {user_name: "user@fs.fed.us", pass_hash: process.env.USERROLE_HASH, user_role: "user", created: "now()", updated: "now()" } 
 		]);
 	},
 	down: function (queryInterface, Sequelize) { 
