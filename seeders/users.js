b7e9ea148bf17606c1a60174fbb1f5ef4b71a4c8
Brian Funk <bfunk@nciinc.com>
2017-04-03 21:19:10 -0400

    change users to env var

--- seeders/users.js
+++ seeders/users.js
@@ -5,13 +5,13 @@ require('dotenv').config();
 module.exports = {
 	up: function (queryInterface, Sequelize) { 
 		return queryInterface.bulkInsert('users', [
-		        {user_name: "admin@fs.fed.us", pass_hash: process.env.ADMINROLE_HASH, user_role: "admin", created: "now()", updated: "now()" }, 
-		        {user_name: "user@fs.fed.us", pass_hash: process.env.USERROLE_HASH, user_role: "user", created: "now()", updated: "now()" } 
+		        {user_name: process.env.ADMINROLE_USER, pass_hash: process.env.ADMINROLE_HASH, user_role: "admin", created: "now()", updated: "now()" }, 
+		        {user_name: process.env.USERROLE_USER, pass_hash: process.env.USERROLE_HASH, user_role: "user", created: "now()", updated: "now()" } 
 		]);
 	},
 	down: function (queryInterface, Sequelize) { 
 		return queryInterface.bulkDelete('users', [
-		        {user_name: ["admin@fs.fed.us","user@fs.fed.us"]} 
+		        {user_name: [process.env.ADMINROLE_USER,process.env.USERROLE_USER]} 
 		]);
 	}
 };
