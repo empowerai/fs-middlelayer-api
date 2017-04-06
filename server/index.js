972f7ad949381f6825b8525025e610f6879d2f10
cwolf1038 <cwolf123456@yahoo.com>
2017-04-06 19:14:23 -0400

    Updated to generate error messages from schema validation.

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ const include = require('include')(__dirname);
 const validateSpecialUse = include('controllers/permits/applications/special-uses/validate.js');
 const dbUtil = include('controllers/permits/applications/special-uses/dbUtil.js');
 const error = include('error.js');
+const errors = require('./patternErrorMessages.json');
 
 const Validator = require('jsonschema').Validator;
 const v = new Validator();
@@ -58,7 +59,210 @@ get.id = function(req, res, pathData){
 
 };
 
-post.app = function(req, res, pathData){
+/**
+ * Removes 'instance' from prop field of validation errors. Used to make fields human readable
+ * 
+ * @param {string} prop - Prop field from validation error
+ * @return {string}
+ */
+function removeInstance(prop){
+
+	let fixedProp = '';
+
+	if (prop.indexOf('.') !== -1){
+
+		fixedProp = prop.substring((prop.indexOf('.') + 1), (prop.length));
+
+	}
+
+	return fixedProp;
+
+}
+
+/**
+ * Combines property and argument fields, if property exists, for missing field errors
+ *
+ * @param  {string}
+ * @param  {string}
+ * @return {string}
+ */
+function combinePropArgument(property, argument){
+
+	let field;
+	if (property.length > 0){
+
+		field = `${property}.${argument}`;
+
+	}
+	else {
+
+		field = argument;
+
+	}
+
+	return field;
+
+}
+/**
+ * Creates error object which can be read by error message building function
+ * 
+ * @param {string} field
+ * @param {string} errorType
+ * @param {string} expectedFieldType
+ * @param {string} enumMessage
+ * @param {string} dependency
+ * @param {array[string]} anyOfFields
+ * 
+ * @return Error object
+ */
+function makeErrorObj(field, errorType, expectedFieldType, enumMessage, dependency, anyOfFields){
+	const output = {
+		field,
+		errorType,
+		expectedFieldType,
+		enumMessage,
+		dependency,
+		anyOfFields
+	};
+	let key;
+	for (key in output){
+		if (output[key] === null){
+			delete output[key];
+		}
+	}
+	return output;
+}
+
+function missingSuperFields(output, field, route){
+
+	const applicantInfo = ['applicantInfo.firstName', 'applicantInfo.lastName', 'applicantInfo.dayPhone.areaCode', 'applicantInfo.dayPhone.number', 'applicantInfo.dayPhone.type', 'applicantInfo.emailAddress', 'applicantInfo.mailingAddress', 'applicantInfo.mailingCity', 'applicantInfo.mailingZIP', 'applicantInfo.mailingState'];
+	if (route === 'tempOutfitters'){
+
+		applicantInfo.push('applicantInfo.orgType');
+
+	}
+	const phone = ['applicantInfo.dayPhone.areaCode', 'applicantInfo.dayPhone.number', 'applicantInfo.dayPhone.type'];
+	const noncommercial = ['noncommercialFields.activityDescription', 'noncommercialFields.locationDescription', 'noncommercialFields.startDateTime', 'noncommercialFields.endDateTime', 'noncommercialFields.numberParticipants'];
+	const tempOutfitter = ['tempOutfitterFields.activityDescription', 'tempOutfitterFields.clientCharges'];
+	
+	if (field === 'applicantInfo'){
+
+		applicantInfo.forEach((missingField)=>{
+
+			output.errorArray.push(makeErrorObj(missingField, 'missing'));
+
+		});
+
+	}
+	else if (field === 'applicantInfo.dayPhone'){
+
+		phone.forEach((missingField)=>{
+
+			output.errorArray.push(makeErrorObj(missingField, 'missing'));
+
+		});
+
+	}
+	else if (field === 'noncommercialFields'){
+
+		noncommercial.forEach((missingField)=>{
+
+			output.errorArray.push(makeErrorObj(missingField, 'missing'));
+
+		});
+
+	}
+	else {
+
+		tempOutfitter.forEach((missingField)=>{
+
+			output.errorArray.push(makeErrorObj(missingField, 'missing'));
+
+		});
+	}
+}
+
+function handleMissingError(output, result, counter){
+	const property = removeInstance(result[counter].property);
+	const field = combinePropArgument(property, result[counter].argument);
+	switch (field){
+	/*
+	case 'applicantInfo':
+	case 'applicantInfo.dayPhone':
+	case 'noncommercialFields':
+	case 'tempOutfitterFields':
+		missingSuperFields(output, field, route);
+	 	break;
+	 */
+	default:
+		output.errorArray.push(makeErrorObj(field, 'missing'));
+		break;
+	}
+}
+
+function handleTypeError(output, result, counter){
+
+	const expectedType = result[counter].argument[0];
+	const property = removeInstance(result[counter].property);
+	output.errorArray.push(makeErrorObj(property, 'type', expectedType));
+
+}
+
+function handleFormatError(output, result, counter){
+
+	const field = `${removeInstance(result[counter].property)}`;
+	output.errorArray.push(makeErrorObj(field, 'format'));
+
+}
+
+function handleEnumError(output, result, counter){
+
+	const property = removeInstance(result[counter].property);
+	output.errorArray.push(makeErrorObj(property, 'enum', null, result[counter].message));
+
+}
+
+function getDependency(result, counter){
+
+	const stackMessage = result[counter].stack;
+	const dependency = stackMessage.split(' property ')[1].split(' not ')[0];
+	return dependency;
+
+}
+
+function handleDependencyError(output, result, counter){
+
+	const error = result[counter];
+	const dependentField = removeInstance(error.argument);
+	const schemaPath = removeInstance(error.property);
+	const dependency = `${schemaPath}.${getDependency(result, counter)}`;
+	output.errorArray.push(makeErrorObj(dependentField, 'dependencies', null, null, dependency));
+
+}
+
+/**
+ * Creates error object for errors resulting from an anyOf section of the validation schema
+ *
+ * @param {object} errorTracking - Error object containing all error to report and the error message to deliver.
+ * @param {array} errorTracking.errorArray - Array contain all errors to report to user.
+ * @param {array} result - Array of errors found during validation.
+ * @param {integer} counter - Position in result that the current error being handled is.
+ * 
+ * @affects errorTracking.errorArray 
+ */
+function handleAnyOfError(errorTracking, result, counter){
+
+	const error = result[counter];
+	const property = removeInstance(error.property);
+	const requiredOptions = [];
+	error.schema.anyOf.forEach((fieldObj)=>{
+		requiredOptions.push(combinePropArgument(property, fieldObj.required[0]));
+	});
+	errorTracking.errorArray.push(makeErrorObj(null, 'anyOf', null, null, null, requiredOptions));
+	
+}
+
+function validateBody(body, pathData){
 	const fileToGet = `server/${pathData.validation.$ref.split('#')[0]}`;
 	const schemaToGet = pathData.validation.$ref.split('#')[1];
 	const applicationSchema = include(fileToGet);
@@ -67,9 +271,188 @@ post.app = function(req, res, pathData){
 		v.addSchema(applicationSchema[key], key);
 	}
 	const val = applicationSchema[schemaToGet];
-	const error = v.validate(req.body, val).errors;
-	const output = {'allgoodpost':{error}};
+	const error = v.validate(body, val).errors;
+	return error;
+}
+
+function processErrors(errors, processedErrors){
+	const length = errors.length;
+	let counter;
+	for (counter = 0; counter < length; counter++){
+
+		if (errors[counter].name === 'required'){
+
+			handleMissingError(processedErrors, errors, counter);
+
+		}
+		else  if (errors[counter].name === 'type'){
+
+			handleTypeError(processedErrors, errors, counter);
+
+		}
+		else if (errors[counter].name === 'format' || errors[counter].name === 'pattern'){
+
+			handleFormatError(processedErrors, errors, counter);
+
+		}
+		else if (errors[counter].name === 'enum'){
+
+			handleEnumError(processedErrors, errors, counter);
+
+		}
+		else if (errors[counter].name === 'dependencies'){
+
+			handleDependencyError(processedErrors, errors, counter);
+
+		}
+		else if (errors[counter].name === 'anyOf'){
+
+			handleAnyOfError(processedErrors, errors, counter);
+
+		}
+	}
+}
+
+function makeFieldReadable(input){
+
+	return input
+	.replace(/([A-Z])/g, ' $1')
+	.replace(/^./, function(str){
+		return str.toUpperCase();
+	})
+	.replace('Z I P', 'Zip')
+	.replace('U R L', 'URL');
+
+}
+
+function makePathReadable(input){
+
+	if (typeof input === 'string'){
+		const parts = input.split('.');
+		const readableParts = [];
+		let readablePath = '';
+		parts.forEach((field)=>{
+			readableParts.push(makeFieldReadable(field));
+		});
+		readablePath = readableParts.shift();
+		readableParts.forEach((part)=>{
+			readablePath = `${readablePath}/${part}`;
+		});
+		return readablePath;
+	}
+	else {
+		return false;
+	}
+
+}
+
+function buildFormatErrorMessage(fullPath){
+	const field = fullPath.substring(fullPath.lastIndexOf('.') + 1);
+	const readablePath = makePathReadable(fullPath);
+	const errorMessage = `${readablePath}${errors[field]}`;
+	return errorMessage;
+
+}
+
+/**
+ * Creates error message for anyOf errors
+ * 
+ * @param  {array[string]} anyOfFields - list of fields, at least one being required.
+ * @return {string}
+ */
+function makeAnyOfMessage(anyOfFields){
+	if (anyOfFields){
+		let output, count = 1;
+		const length = anyOfFields.length;
+		output = `${makePathReadable(anyOfFields[0])}`;
+		while (count < length) {
+			const field = anyOfFields[count];
+			output = `${output} or ${makePathReadable(field)}`;
+			count ++;
+		}
+		return output;
+	}
+	else {
+		return false;
+	}
+}
+
+function concatErrors(errorMessages){
+
+	let output = '';
+	errorMessages.forEach((message)=>{
+		output = `${output}${message} `;
+	});
+	output = output.trim();
+	return output;
+}
+
+function generateErrors(output){
+
+	let errorMessage = '';
+	const messages = [];
+	output.errorArray.forEach((error)=>{
+
+		const missing = `${makePathReadable(error.field)} is a required field.`;
+		const type = `${makePathReadable(error.field)} is expected to be type '${error.expectedFieldType}'.`;
+		const enumMessage = `${makePathReadable(error.field)} ${error.enumMessage}.`;
+		const dependencies = `Having ${makePathReadable(error.field)} requires that ${makePathReadable(error.dependency)} be provided.`;
+		const anyOf = `Either ${makeAnyOfMessage(error.anyOfFields)} is a required field.`;
+
+		switch (error.errorType){
+		case 'missing':
+			messages.push(missing);
+			error.message = missing;
+			break;
+		case 'type':
+			messages.push(type);
+			error.message = type;
+			break;
+		case 'format':
+		case 'pattern':
+			messages.push(buildFormatErrorMessage(error.field));
+			error.message = buildFormatErrorMessage(error.field);
+			break;
+		case 'enum':
+			messages.push(enumMessage);
+			error.message = enumMessage;
+			break;
+		case 'dependencies':
+			messages.push(dependencies);
+			error.message = dependencies;
+			break;
+		case 'anyOf':
+			messages.push(anyOf);
+			error.message = anyOf;
+			break;
+		}
+
+	});
+	errorMessage = concatErrors(messages);
+	return errorMessage;
+
+}
+
+post.app = function(req, res, pathData){
+	
 	//console.log(`output: \n${JSON.stringify(output)}`)
+	const errors = validateBody(req.body, pathData);
+	const processedErrors = {
+		errorArray:[]
+	};
+	let errorMessage;
+	if (errors.length > 0){
+		processErrors(errors, processedErrors);
+		errorMessage = generateErrors(processedErrors);
+	}
+	const output = {
+		'request':req.body,
+		'length':errors.length,
+		'errors':{
+			allErrors:processedErrors,
+			message:errorMessage
+		}
+	};
 	return output;
 };
 //*******************************************************************
