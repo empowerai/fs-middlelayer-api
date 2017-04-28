ae1c19742e765811c154efe63fd0a9b860221397
Shekar <spendem@computechinc.com>
2017-04-28 13:06:48 -0400

    more functional test cases.

--- test/functions-test.js
+++ test/functions-test.js
@@ -60,6 +60,47 @@ describe('Function Tests: validation.js ', function(){
 		.to.be.equal('xyz');
 	});
 
+	it('combinePropArgument should return instance and property', function(){
+		expect( validationJs.functions.combinePropArgument('abc', 'xyz') )
+		.to.be.equal('abc.xyz');
+	});
+
+	it('combinePropArgument should return property when instance is blank', function(){
+		expect( validationJs.functions.combinePropArgument('', 'xyz') )
+		.to.be.equal('xyz');
+	});
+
+	it('makeErrorObj should return output object with supplied elements', function(){
+		expect( validationJs.functions.makeErrorObj('field', 'errorType', 'expectedFieldType', 'enumMessage', 'dependency', 'anyOfFields') )
+		.to.eql({ field: 'field', errorType:'errorType', expectedFieldType:'expectedFieldType', enumMessage:'enumMessage', dependency:'dependency', anyOfFields:'anyOfFields'});
+	});
+
+	it('makeErrorObj should return output object with supplied elements (not all)', function(){
+		expect( validationJs.functions.makeErrorObj('field', 'errorType', 'expectedFieldType', null, 'dependency', 'anyOfFields') )
+		.to.eql({ field: 'field', errorType:'errorType', expectedFieldType:'expectedFieldType', dependency:'dependency', anyOfFields:'anyOfFields'});
+	});
+
+	it('concatErrors should return expected output', function(){
+		expect( validationJs.functions.concatErrors(['a','b']) )
+		.to.be.equal('a b');
+	});
+
+	it('makePathReadable should return expected output', function(){
+		expect( validationJs.functions.makePathReadable('error.field') )
+		.to.be.equal('Error/Field');
+	});
+
+	it('makeAnyOfMessage should return expected output', function(){
+		expect( validationJs.functions.makeAnyOfMessage(['field1', 'field2']) )
+		.to.be.equal('Field1 or Field2');
+	});
+
+	it('makeFieldReadable should return expected output', function(){
+		expect( validationJs.functions.makeFieldReadable('firstName') )
+		.to.be.equal('First Name');
+	});
+	
+
 });
 
 //*******************************************************************
