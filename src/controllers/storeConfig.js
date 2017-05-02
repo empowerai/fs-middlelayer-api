bb1d88889bb3bae8b9b1c9bbf808baf83bb854e2
cwolf1038 <cwolf123456@yahoo.com>
2017-05-02 17:19:56 -0400

    Fixed lint issues

--- src/controllers/storeConfig.js
+++ src/controllers/storeConfig.js
@@ -22,7 +22,6 @@ const AWS = require('aws-sdk');
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 const AWS_REGION = process.env.AWS_REGION;
-const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
 
 AWS.config.update({
 	accessKeyId: AWS_ACCESS_KEY_ID,
