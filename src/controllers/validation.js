48399d1cad3a1d946124d02a7754f5fb64bca693
cwolf1038 <cwolf123456@yahoo.com>
2017-05-03 17:20:42 -0400

    Fix swagger issues

--- src/controllers/validation.js
+++ src/controllers/validation.js
@@ -344,7 +344,7 @@ function handleAnyOfError(errorTracking, result, counter){
  * @return {Object} schemas - fullSchema is the full validation schemas for all permit types. schemaToUse is the validation schema for this route
  */
 function getValidationSchema(pathData){
-	const fileToGet = `src/controllers/${pathData['x-validation'].$ref.split('#')[0]}`;
+	const fileToGet = `src/${pathData['x-validation'].$ref.split('#')[0]}`;
 	const schemaToGet = pathData['x-validation'].$ref.split('#')[1];
 	const applicationSchema = include(fileToGet);
 	return {
