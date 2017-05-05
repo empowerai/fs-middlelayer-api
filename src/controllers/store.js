8b087dde14b28bd4521a0d6d85537b476a9f07c8
Shekar <spendem@computechinc.com>
2017-05-05 10:00:21 -0400

    implemented zip download route for outfitter files.

--- src/controllers/store.js
+++ src/controllers/store.js
@@ -15,6 +15,7 @@
 // required modules
 
 const config = require('./storeConfig.js');
+const s3zipper = require ('aws-s3-zipper');
 
 //*************************************************************
 // AWS
@@ -76,5 +77,82 @@ function getFile(controlNumber, fileName, callback){
 	});
 }
 
+/**
+ * Retreives file from S3
+ * @param  {Number}	  controlNumber - controlNumber of application files is associated with
+ * @param  {Array}	  dbFiles - database file objects associated with that controlNumber.
+ * @param  {Object}	  res - response object
+ * @param  {Function} callback      - function to call after files have been retreived, or error returned
+ */
+function getFilesZip(controlNumber, dbFiles, res, callback){
+
+	const zipper = new s3zipper(config.getStoreConfig());
+
+	const filePath = `${controlNumber}`;
+
+	const storeFiles = [];
+	const fileNames = [];
+
+	dbFiles.forEach((dbFile)=>{
+		fileNames.push(dbFile.filePath);
+	});
+
+	zipper.getFiles({
+		folderName: filePath
+	},
+    function (err, fileResult) {
+	if (err){
+		console.error(err);
+		return callback(err);
+	}
+	else {
+			
+		if (fileResult.files.length === 0 ){
+			return callback('files not found');
+		}
+		else {
+				
+			fileResult.files.forEach((storeFile)=>{
+
+				storeFiles.push(storeFile.Key);
+
+			});	
+
+			zipper.filterOutFiles = function(file){
+				if (fileNames.indexOf(file.Key) >= 0){
+					return file;
+				}
+				else {
+					return null;
+				}
+			};
+
+			res.set('Content-Type', 'application/zip');
+			res.set('Content-Disposition', 'attachment; filename=' + controlNumber + '.zip');
+
+			zipper.streamZipDataTo({
+				folderName: filePath,
+				pipe: res,
+				recursive: true
+			},
+				function (err, result) {
+					if (err){
+						console.error(err);
+						return callback(err);
+					}
+					else {
+						return callback(null);	
+					}
+				}
+			);
+
+		}
+		
+	}
+});
+
+}
+
 module.exports.getFile = getFile;
 module.exports.uploadFile = uploadFile;
+module.exports.getFilesZip = getFilesZip;
--- src/controllers/storeConfig.js
+++ src/controllers/storeConfig.js
@@ -22,12 +22,24 @@ const AWS = require('aws-sdk');
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 const AWS_REGION = process.env.AWS_REGION;
+const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
 
 AWS.config.update({
 	accessKeyId: AWS_ACCESS_KEY_ID,
 	secretAccessKey: AWS_SECRET_ACCESS_KEY,
-	region: AWS_REGION
+	region: AWS_REGION,
+	bucket: AWS_BUCKET_NAME
 });
+
+function getStoreConfig(){
+	const config = {
+		accessKeyId: AWS_ACCESS_KEY_ID,
+		secretAccessKey: AWS_SECRET_ACCESS_KEY,
+		region: AWS_REGION,
+		bucket: AWS_BUCKET_NAME
+	};
+	return config;
+}
 
 function getStoreObject(){
 	const s3 = new AWS.S3();
@@ -35,3 +47,4 @@ function getStoreObject(){
 }
 
 module.exports.getStoreObject = getStoreObject;
+module.exports.getStoreConfig = getStoreConfig;
