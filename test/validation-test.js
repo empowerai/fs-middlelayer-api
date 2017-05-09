bc0f1034a8b0a82c06b32c82003fadaa870c464c
cwolf1038 <cwolf123456@yahoo.com>
2017-05-09 17:30:04 -0400

    Updated tests that should be unit tests to be unit tests

--- test/validation-test.js
+++ test/validation-test.js
@@ -13,6 +13,7 @@
 
 //*******************************************************************
 
+const AWS = require('mock-aws');
 const chai = require('chai');
 const expect = chai.expect;
 
@@ -45,8 +46,21 @@ const noncommercialFactory = factory.factory(testNoncommercialBody);
 // Saving to DB working
 // Retreiving from DB working
 // Authorization working
-// 
-// 
+
+before(function(){
+	console.log('process.env.npm_config_mock');
+	console.log(process.env.npm_config_mock);
+	if (process.env.npm_config_mock === 'Y'){
+		AWS.mock('S3', 'putObject', { ETag: '"82e8674bebaea2797c28872c9a38ad43"' });
+		AWS.mock('S3', 'getObject', testObjects.mockS3Get);
+	}
+});
+
+after(function(){
+	if (process.env.npm_config_mock === 'Y'){
+		AWS.restore();
+	}
+});
 
 describe('outfitters validation ', function(){
 	describe('ensure field is present', function(){
@@ -68,6 +82,12 @@ describe('outfitters validation ', function(){
 			)
 			.to.be.equal(3);
 		});
+		it('should report issues when no applicantInfo/org type is provided', function(){
+			expect (
+				specialUses.validate.validateBody(tempOutfitterFactory.create({'applicantInfo.orgType' : undefined}), outfittersObjects.pathData, outfittersObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
 		it('should report issues when no tempOutfitterFields/activity description is provided', function(){
 			expect (
 				specialUses.validate.validateBody(tempOutfitterFactory.create({'tempOutfitterFields.activityDescription' : undefined}), outfittersObjects.pathData, outfittersObjects.derefSchema).errorArray.length
@@ -88,13 +108,13 @@ describe('outfitters validation ', function(){
 		});
 	});
 	describe('ensure fields are the right type', function(){
-		it('should report issues when no tempOutfitterFields/activity description is provided', function(){
+		it('should report issues when when the wrong type of tempOutfitterFields/activity description is provided', function(){
 			expect (
 				specialUses.validate.validateBody(tempOutfitterFactory.create({'tempOutfitterFields.activityDescription' : 123}), outfittersObjects.pathData, outfittersObjects.derefSchema).errorArray.length
 			)
 			.to.be.equal(1);
 		});
-		it('should report issues when no tempOutfitterFields/client charges is provided', function(){
+		it('should report issues when when the wrong type of tempOutfitterFields/client charges is provided', function(){
 			expect (
 				specialUses.validate.validateBody(tempOutfitterFactory.create({'tempOutfitterFields.clientCharges' : 500}), outfittersObjects.pathData, outfittersObjects.derefSchema).errorArray.length
 			)
@@ -232,18 +252,171 @@ describe('noncommercial validation', function(){
 			.to.be.equal(1);
 		});
 	});
-	/*
-	it('', function(){
-		const output = specialUses.validate.validateBody(tempOutfitterFactory.create({tempOutfitterFields : undefined}), testObjects.pathData, testObjects.derefSchema);
-		//expect (output).to.be.equal('Applicant Info/First Name is a required field.');
-    
+	describe('ensure fields are the right type', function(){
+		it('should report issues when the wrong type of applicantInfo/first name is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.firstName' : 123}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong type of applicantInfo/last name is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.lastName' : 123}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong type of applicantInfo/day phone/area code is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.dayPhone.areaCode' : '123'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong type of applicantInfo/day phone/number is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.dayPhone.number' : '123'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong type of applicantInfo/day phone/phone type is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.dayPhone.phoneType' : 1}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong type of applicantInfo/day phone/extension is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.dayPhone.extension' : 1}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong type of applicantInfo/email address is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.emailAddress' : 123}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong type of applicantInfo/mailing address is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.mailingAddress' : 123}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong type of applicantInfo/mailing city is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.mailingCity' : 123}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong type of applicantInfo/mailing state is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.mailingState' : 123}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong type of applicantInfo/mailing zip is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.mailingZIP' : 123}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong type of noncommcialFields/activity description is provided', function(){
+			expect (
+				specialUses.validate.validateBody(tempOutfitterFactory.create({'tempOutfitterFields.activityDescription' : 123}), outfittersObjects.pathData, outfittersObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
 	});
-	*/
+	describe('ensure fields are the right format', function(){
+		
+		it('should report issues when the wrong format of applicantInfo/day phone/area code is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.dayPhone.areaCode' : 12}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong format of applicantInfo/day phone/number is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.dayPhone.areaCode' : 12}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong format of applicantInfo/mailing state is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.mailingState' : 'ORE'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong format of applicantInfo/mailing zip is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.mailingZIP' : '123456'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should not report issues when the right format of applicantInfo/mailing zip is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.mailingZIP' : '123456789'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(0);
+		});
+		it('should report issues when the wrong format of region is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'region' : '123'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong format of forest is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'forest' : '123'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong format of district is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'district' : '123'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong format of noncommercialFields/start date time is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'noncommercialFields.startDateTime' : '01-12-2012'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues when the wrong format of noncommercialFields/end date time is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'noncommercialFields.endDateTime' : '01-12-2012'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+	});
+	describe('ensure fields follow their pattern', function(){
+		it('should report issues for invalid pattern for email address is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.emailAddress' : 'invalid'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+	});
+	describe('ensure fields with enumuration are validated', function(){
+		it('should report issues for invalid option for type is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'type' : 'invalid'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+		it('should report issues for invalid option for applicant info/org type is provided', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.orgType' : 'invalid'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+	});
+	describe('ensure fields with a dependency are checked', function(){
+		it('should report issues for ', function(){
+			expect (
+				specialUses.validate.validateBody(noncommercialFactory.create({'applicantInfo.organizationName' : 'theOrg'}), noncommercialObjects.pathData, noncommercialObjects.derefSchema).errorArray.length
+			)
+			.to.be.equal(1);
+		});
+	});
 });
-/*
-describe('validation: validate file',function(){
-	it('should', function(){
-
-	};
-});
-*/
